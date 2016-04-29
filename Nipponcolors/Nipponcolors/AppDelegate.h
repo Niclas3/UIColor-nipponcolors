948f94261f8787275b689e357ace46f4fc95e398
zhoumin <zhoumin@jsdttec.com>
2016-04-29 13:55:09 +0800

    init project

---
+++ Nipponcolors/Nipponcolors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nipponcolors
+//
+//  Created by nic on 16/4/29.
+//  Copyright © 2016年 nic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nipponcolors/Nipponcolors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nipponcolors
+//
+//  Created by nic on 16/4/29.
+//  Copyright © 2016年 nic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
